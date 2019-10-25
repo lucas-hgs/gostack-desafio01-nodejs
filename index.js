e6edb387979cbe3abf7cbbc0dc4687d4fdb7dd28
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2019-10-25 00:54:58 -0300

    Created all the routes

--- index.js
+++ index.js
@@ -2,4 +2,55 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+const projects = [];
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const { task } = req.body;
+
+  const result = projects.find(x => x.id === id);
+  result.tasks.push(task);
+
+  return res.json(result);
+});
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const result = projects.find(x => x.id === id);
+  result.title = title;
+
+  return res.json(result);
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const result = projects.findIndex(x => x.id === id);
+  projects.splice(result, 1);
+
+  return res.json(projects);
+});
+
 server.listen(3000);
